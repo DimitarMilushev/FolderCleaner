15788cc0b3d786a7097c548ddcaecec7ab3632d0
DimitarMilushev <DeadlyCookye@gmail.com>
2019-03-03 12:15:55 +0200

    Files upload.

---
+++ MyFolderCleaner/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFolderCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           const string path = @"C:\Users\DeadlyCookie\Downloads";
+
+            IEnumerable<FileInfo> directoryFiles = new DirectoryInfo(path).EnumerateFiles();
+
+            if (directoryFiles.Count() == 0)
+                return;
+            else
+            {
+                foreach (var file in directoryFiles)
+                {
+                    file.Delete();
+                }
+            }          
+        }
+    }
+}
